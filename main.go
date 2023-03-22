6a14bc07250d73d49b1883093195621464a12c99
nate smith <vilmibm@github.com>
2023-03-21 21:56:12 -0700

    add input loop, dummy text

--- main.go
+++ main.go
@@ -3,11 +3,45 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/gdamore/tcell/v2"
 )
 
-func _main() (err error) {
+const animInterval = time.Millisecond * 100
+
+/*
+	the overall goal of this program is to interleave a set of text files
+	together into a smudge stick and then burn them away character by character.
+	the purpose of this program is to aid a user in meditating over their
+	computer as a place and a space as opposed to just a tool or content delivery
+	mechanism.
+
+	input files are listed ad naueseum as positional arguments.
+*/
+
+const dummyText = `Celia agreed with her sister, but she did not say so. The two little
+girls had been sitting by the fireside, for the April evening
+was chilly; but now the daylight had nearly faded, and Joy, rising,
+went to the door and peeped into the passage to make certain that
+Jane had lit the gas there. Satisfied on that point, she returned
+to her former seat by the fire, and continued the conversation.
+
+"I wonder if we ought to send Jane to the drawing-room to light
+the gas?" Celia suggested presently. "But, no, mother would be sure
+to ring if she wished it. Oh, the gentleman's going at last!"
+
+There was a sound of footsteps in the passage. The front door opened
+and shut, and the next minute Mrs. Wallis joined her little
+daughters. She was a tall, stately woman with a pale, handsome face,
+and hair which was prematurely grey.
+
+"My visitor kept me some time," she remarked, as she seated herself
+in an easy chair, and glanced from one to the other of the children.
+"I suppose you have been cogitating about him, and wondering who he
+could possibly be?"`
+
+func _main(input string) (err error) {
 	s, err := tcell.NewScreen()
 	if err != nil {
 		return err
@@ -19,36 +53,62 @@ func _main() (err error) {
 
 	defer s.Fini()
 
-	defStyle := tcell.StyleDefault.Background(tcell.ColorReset).Foreground(tcell.ColorReset)
+	defStyle := tcell.StyleDefault.Background(tcell.ColorBlack).Foreground(tcell.ColorWhite)
 	s.SetStyle(defStyle)
 
-	s.Clear()
+	quit := make(chan struct{})
+	go inputLoop(s, quit)()
 
-	s.SetContent(0, 0, 'H', nil, defStyle)
-	s.SetContent(1, 0, 'i', nil, defStyle)
-	s.SetContent(2, 0, '!', nil, defStyle)
-
+	var quitting bool
 	for {
-		// Update screen
+		select {
+		case <-quit:
+			quitting = true
+		case <-time.After(animInterval):
+		}
+
+		if quitting {
+			break
+		}
+
+		s.Clear()
+
+		// TODO figure out width of stick
+		// TODO draw text starting at y = 0, going to height
+		// TODO burn animation
+
+		s.SetContent(0, 0, 'H', nil, defStyle)
+		s.SetContent(1, 0, 'i', nil, defStyle)
+		s.SetContent(2, 0, '!', nil, defStyle)
+
 		s.Show()
+	}
 
-		// Poll event
-		ev := s.PollEvent()
+	return nil
+}
 
-		// Process event
-		switch ev := ev.(type) {
-		case *tcell.EventResize:
-			s.Sync()
-		case *tcell.EventKey:
-			if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
-				return nil
+func inputLoop(s tcell.Screen, quit chan struct{}) func() {
+	return func() {
+		for {
+			s.Show()
+
+			ev := s.PollEvent()
+
+			switch ev := ev.(type) {
+			case *tcell.EventResize:
+				s.Sync()
+			case *tcell.EventKey:
+				if ev.Key() == tcell.KeyEscape || ev.Key() == tcell.KeyCtrlC {
+					close(quit)
+				}
 			}
 		}
 	}
 }
 
 func main() {
-	err := _main()
+	// TODO parse each argument, read file, do the interleaving
+	err := _main(dummyText)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, err.Error())
 		os.Exit(1)
